2248933ed6555271e0ee377c3f030a2228fdc75a
Wojtek <wojciech_lisowski@wp.pl>
2025-03-26 11:58:03 +0100

    Update register.blade.php

--- todo/resources/views/auth/register.blade.php
+++ todo/resources/views/auth/register.blade.php
@@ -38,7 +38,7 @@
     }
 
     .form-signin input[type="password"] {
-    margin-bottom: 10px;
+    /* margin-bottom: 10px; */
     border-top-left-radius: 0;
     border-top-right-radius: 0;
     }
@@ -51,7 +51,8 @@
 @section("content")
 
 <main class="form-signin">
-  <form>
+  <form method="POST" action="{{route("register.post")}}">
+  @csrf
     {{-- <img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> --}}
     <h1 class="h3 mb-3 fw-normal">
     Create your account:</h1>
@@ -60,27 +61,30 @@
       <input name="name" type="text" class="form-control" id="floatingInput" placeholder="Enter name">
       <label for="floatingInput">
       Full name</label>
+      @error('name')
+        <span class="text-danger">{{ $message }}</span>
+      @enderror
     </div>
-
 
     <div class="form-floating">
       <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
       <label for="floatingInput">
       Email address</label>
+      @error('email')
+        <span class="text-danger">{{ $message }}</span>
+      @enderror
     </div>
+
     <div class="form-floating">
       <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">
       Password</label>
+      @error('password')
+        <span class="text-danger">{{ $message }}</span>
+      @enderror
     </div>
 
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> 
-        Remember me
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Register</button>
     <p class="mt-5 mb-3 text-muted">© 2025</p>
   </form>
 </main>
